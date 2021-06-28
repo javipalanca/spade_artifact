b1c6f8d6a0fc5ff6f967726ad79222e47d1b1cd1
jpalanca <jpalanca@dsic.upv.es>
2021-06-28 15:36:07 +0200

    Added more basic tests.

--- tests/test_artifact.py
+++ tests/test_artifact.py
@@ -14,6 +14,48 @@ def test_run():
     assert artifact.get("test_passed")
 
 
+def test_setup():
+    class TestArtifact(MockedConnectedArtifact):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.value = False
+
+        async def setup(self):
+            self.value = True
+
+    artifact = TestArtifact(jid="fake@jid", password="fake_password")
+
+    assert artifact.value is False
+    artifact.start()
+    assert artifact.value
+
+
+def test_name():
+    artifact = MockedConnectedArtifactFactory()
+    assert artifact.name == "fake"
+
+
+def test_is_alive():
+    artifact = MockedConnectedArtifactFactory()
+    assert artifact.is_alive() is False
+    artifact.start()
+    assert artifact.is_alive()
+
+
+def test_set_get():
+    artifact = MockedConnectedArtifactFactory()
+
+    assert artifact.get("A_KEY") is None
+
+    artifact.set("A_KEY", True)
+
+    assert artifact.get("A_KEY")
+
+    artifact.set("A_KEY", 1234)
+
+    assert artifact.get("A_KEY") == 1234
+
+
 def test_send_msg():
     class A(MockedConnectedArtifact):
         async def run(self):
@@ -43,3 +85,23 @@ def test_receive():
     artifact.start()
 
     assert artifact.msg == Message()
+
+
+def test_mailbox_size():
+    class A(MockedConnectedArtifact):
+        async def run(self):
+            self.client = Mock()
+            self.client.send = CoroutineMock()
+            self.msg = await self.receive(1)
+
+    artifact = A(jid="fakejid", password="fakesecret")
+
+    artifact._message_received(Message().prepare())
+
+    assert artifact.mailbox_size() == 1
+
+    artifact.start()
+
+    assert artifact.msg == Message()
+
+    assert artifact.mailbox_size() == 0
