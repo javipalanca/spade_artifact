4eceff9073a9a1fc8890af6a4e4946cf0657b514
manel <manelbng@gmail.com>
2024-03-04 12:25:47 +0100

    add default values in init

--- examples/read_csv.py
+++ examples/read_csv.py
@@ -10,19 +10,19 @@ from spade_artifact import ArtifactMixin
 
 
 class CSVReaderArtifact(spade_artifact.Artifact):
-    def __init__(self, jid, passwd, csv_file, columns, frequency,time_column):
+    def __init__(self, jid, passwd, csv_file, columns=None, frequency=1, time_column=None):
         super().__init__(jid, passwd)
         self.csv_file = csv_file
         self.columns = columns
-        self.frequency = frequency
         self.time_column = time_column
+        self.frequency = frequency
 
     async def setup(self):
         self.presence.set_available()
 
     async def run(self):
         self.presence.set_available()
-        df = pd.read_csv(self.csv_file, usecols=self.columns)
+        df = pd.read_csv(self.csv_file, usecols=self.columns if self.columns else None)
 
         last_time = None
         for index, row in df.iterrows():
@@ -76,7 +76,7 @@ async def main():
     columns_to_read = config["columns_to_read"]
 
     time_column = config.get("time_column", None)
-    read_frequency = None if time_column else config.get("read_frequency", 1)
+    read_frequency = config.get("read_frequency", 1)
 
 
     artifact = CSVReaderArtifact(artifact_jid, artifact_passwd, csv_file_path, columns_to_read,
