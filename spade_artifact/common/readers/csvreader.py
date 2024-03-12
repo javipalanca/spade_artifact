c8a995d149c557193c8f4c4a76dfaf0ed2c8a90b
manel <manelbng@gmail.com>
2024-03-12 11:26:12 +0100

    documentation CSVReaderArtifact added  and test updated

--- spade_artifact/common/readers/csvreader.py
+++ spade_artifact/common/readers/csvreader.py
@@ -3,18 +3,45 @@ import asyncio
 import spade_artifact
 
 class CSVReaderArtifact(spade_artifact.Artifact):
+    """
+    An artifact for asynchronously reading and processing data from CSV files.
+
+    This artifact reads data from a CSV file and publishes each row at regular intervals or based on a time column.
+
+    Attributes:
+        csv_file (str): Path to the CSV file to be read.
+        columns (list[str], optional): A list of column names to read from the CSV file. If None, all columns are read.
+        frequency (int, optional): The frequency in seconds at which rows are published if no time_column is specified. Defaults to 1.
+        time_column (str, optional): The name of the column that contains timestamp information. If specified, rows are published based on the time difference between rows instead of the fixed frequency.
+
+    Args:
+        jid (str): The JID (Jabber Identifier) of the artifact.
+        passwd (str): The password for the artifact to authenticate with the XMPP server.
+        csv_file (str): Path to the CSV file to be read.
+        columns (list[str], optional): A list of column names to read from the CSV file. Defaults to None.
+        frequency (int, optional): The frequency in seconds at which rows are published if no time_column is specified. Defaults to 1.
+        time_column (str, optional): The name of the column that contains timestamp information.
+
+    """
     def __init__(self, jid, passwd, csv_file, columns=None, frequency=1, time_column=None):
         super().__init__(jid, passwd)
         self.csv_file = csv_file
         self.columns = columns
-        self.time_column = time_column
         self.frequency = frequency
+        self.time_column = time_column
 
     async def setup(self):
+        """
+        Sets up the artifact by marking its presence as available on the XMPP server.
+        """
         self.presence.set_available()
 
-
     async def run(self):
+        """
+        Starts the artifact's main operation of reading from the CSV and publishing rows.
+
+        This method reads the CSV file row by row, and if a time_column is specified, it waits for the time difference between the current and last row before publishing the next row. If no time_column is specified, it publishes rows at a fixed frequency defined by the `frequency` attribute.
+        """
         self.presence.set_available()
         df = pd.read_csv(self.csv_file, usecols=self.columns if self.columns else None)
 
