22a49ed300c5f448e8ec122a433c0ec4584d296a
manel <manelbng@gmail.com>
2024-11-25 11:41:32 +0100

    update test artifacts to async functions

--- tests/test_artifact.py
+++ tests/test_artifact.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Tests for `spade_artifact` package."""
-from asynctest import Mock, CoroutineMock
+import pytest
+import asyncio
 from spade.message import Message
-
+from aioxmpp import JID
+from asynctest import Mock, CoroutineMock
 from tests.factories import MockedConnectedArtifactFactory, MockedConnectedArtifact
 
 
-def test_run():
+@pytest.mark.asyncio
+async def test_run(event_loop):
     artifact = MockedConnectedArtifactFactory()
-    future = artifact.start()
-    future.result()
-    artifact.join()
+    artifact.loop = event_loop
+    await artifact.start()
+    await artifact._async_join(timeout=1)
     assert artifact.get("test_passed")
 
 
-def test_setup():
+@pytest.mark.asyncio
+async def test_setup(event_loop):
     class TestArtifact(MockedConnectedArtifact):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
@@ -29,11 +29,10 @@ def test_setup():
             self.kill()
 
     artifact = TestArtifact(jid="fake@jid", password="fake_password")
-
+    artifact.loop = event_loop
     assert artifact.value is False
-    future = artifact.start()
-    future.result()
-    artifact.join()
+    await artifact.start()
+    await artifact._async_join(timeout=1)
     assert artifact.value
 
 
@@ -42,29 +41,27 @@ def test_name():
     assert artifact.name == "fake"
 
 
-def test_is_alive():
+@pytest.mark.asyncio
+async def test_is_alive(event_loop):
     artifact = MockedConnectedArtifactFactory()
+    artifact.loop = event_loop
     assert artifact.is_alive() is False
-    future = artifact.start()
-    future.result()
+    await artifact.start()
     assert artifact.is_alive()
+    await artifact.stop()
 
 
 def test_set_get():
     artifact = MockedConnectedArtifactFactory()
-
     assert artifact.get("A_KEY") is None
-
     artifact.set("A_KEY", True)
-
     assert artifact.get("A_KEY")
-
     artifact.set("A_KEY", 1234)
-
     assert artifact.get("A_KEY") == 1234
 
 
-def test_send_msg():
+@pytest.mark.asyncio
+async def test_send_msg(event_loop):
     class A(MockedConnectedArtifact):
         async def run(self):
             self.client = Mock()
@@ -74,15 +71,17 @@ def test_send_msg():
             self.kill()
 
     artifact = A(jid="fakejid", password="fakesecret")
+    artifact.loop = event_loop
+    await artifact.start()
+    await artifact._async_join(timeout=1)
+    artifact.client.send.assert_called_once()
+    actual_call = artifact.client.send.call_args[0][0]
+    expected_jid = JID.fromstr("fakejid")
+    assert actual_call.from_ == expected_jid
 
-    future = artifact.start()
-    future.result()
-    artifact.join()
 
-    assert artifact.client.send.called_with(Message().prepare())
-
-
-def test_receive():
+@pytest.mark.asyncio
+async def test_receive(event_loop):
     class A(MockedConnectedArtifact):
         async def run(self):
             self.client = Mock()
@@ -91,35 +90,38 @@ def test_receive():
             self.kill()
 
     artifact = A(jid="fakejid", password="fakesecret")
-
-    artifact._message_received(Message().prepare())
-
-    future = artifact.start()
-    future.result()
-    artifact.join()
-
-    assert artifact.msg == Message()
-
-
-def test_mailbox_size():
-    class A(MockedConnectedArtifact):
-        async def run(self):
-            self.client = Mock()
-            self.client.send = CoroutineMock()
-            self.msg = await self.receive(1)
-            self.kill()
-
-    artifact = A(jid="fakejid", password="fakesecret")
+    artifact.loop = event_loop
+    await artifact.start()
 
     future = artifact._message_received(Message().prepare())
-    future.result()
+    await asyncio.wrap_future(future)
+    await artifact._async_join(timeout=1)
 
+    assert hasattr(artifact, 'msg')
+    received_msg = artifact.msg
+    expected_msg = Message()
+    assert received_msg.to == expected_msg.to
+    assert received_msg.thread == expected_msg.thread
+    assert received_msg.metadata == expected_msg.metadata
+
+
+@pytest.mark.asyncio
+async def test_mailbox_size(event_loop):
+    class A(MockedConnectedArtifact):
+        async def run(self):
+            self.client = Mock()
+            self.client.send = CoroutineMock()
+            self.msg = await self.receive(1)
+            self.kill()
+
+    artifact = A(jid="fakejid", password="fakesecret")
+    artifact.loop = event_loop
+    await artifact.start()
+    assert artifact.mailbox_size() == 0
+
+    msg = Message().prepare()
+    await artifact.queue.put(Message.from_node(msg))
     assert artifact.mailbox_size() == 1
 
-    future = artifact.start()
-    future.result()
-    artifact.join()
-
-    assert artifact.msg == Message()
-
+    await artifact._async_join(timeout=1)
     assert artifact.mailbox_size() == 0
